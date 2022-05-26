4e5f876148c8f46813f0297c9816d0e2ad5d5b91
lazaroofarrill <lazaroofarrill@gmail.com>
2022-05-26 01:18:03 -0400

    inject configService where needed

---
+++ src/modules/auth/guards/roles.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { Role } from '../../profile/constants/role'
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(...allowedRoles: Role[]) {
+    this.allowedRoles = allowedRoles
+  }
+  private readonly allowedRoles
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const userRoles = context.switchToHttp().getRequest().user.roles as Role[]
+    for (const r of this.allowedRoles) {
+      if (userRoles.includes(r)) return true
+    }
+
+    return false
+  }
+}
